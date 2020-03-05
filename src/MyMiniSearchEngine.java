15335c72b9ac343419e6f8bfcd82a54c310811ec
psantos-stack <righthandspy@gmail.com>
2020-03-05 14:57:03 -0800

    Completed assignment

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,17 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.*;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
     // do not change the signature of index()
     private Map<String, List<List<Integer>>> indexes;
-
+    private int numOfDocs;
     // disable default constructor
     private MyMiniSearchEngine() {
     }
-
+    public Map<String, List<List<Integer>>> getIndexes(){
+        return indexes;
+    }
     public MyMiniSearchEngine(List<String> documents) {
+        numOfDocs = documents.size();
         index(documents);
     }
 
@@ -19,13 +22,120 @@ public class MyMiniSearchEngine {
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
         //homework
+        indexes = new HashMap<>();
+        //will contain all unique words mapped to a vector containing indexes
+        Set<String> uniqueKeySet = new HashSet<>();
+        for(int i = 0; i < texts.size(); i++){
+            uniqueKeySet.addAll(getAllUniqueWordsInADoc(texts.get(i)));
+        }
+        List<String> uniqueKeyList = new ArrayList<>(uniqueKeySet);
+        for(int i = 0; i < uniqueKeyList.size(); i++){
+            ArrayList<List<Integer>> listOfDocs = new ArrayList<>(texts.size());
+            for(int g = 0; g < texts.size(); g++){
+                listOfDocs.add(new LinkedList<>());
+            }
+            indexes.put(uniqueKeyList.get(i), listOfDocs);
+        }
+        //here, we have all words mapped to an empty index vector
+        for(int i = 0; i < texts.size(); i++){
+            String[] allWordsInDoc = texts.get(i).toLowerCase().split(" ");
+            //bring everything in the doc down to lower case to avoid errors as well
+            for(int g = 0; g < allWordsInDoc.length; g++){
+                List<Integer> positionVectorOfIthWord = indexes.get(allWordsInDoc[g]).get(i);
+                positionVectorOfIthWord.add(g);
+            }
+        }
     }
-
+    private Set<String> getAllUniqueWordsInADoc(String fullString){
+        //we will directly manipulate fullString since Java is pass-by-value
+        return new HashSet<>(Arrays.asList(fullString.toLowerCase().split(" ")));
+    }
     // search(key) return all the document ids where the given key phrase appears.
     // key phrase can have one or two words in English alphabetic characters.
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
         // homework
-        return new ArrayList<>(); // place holder
+        String[] wordsInKeyPhrase = keyPhrase.toLowerCase().split(" ");
+        //here, we have a sequence of keys
+        List<Integer> documentsWithTermFound = new LinkedList<>();
+        for(int i = 0; i < numOfDocs; i++){
+            if(termFoundInDoc(wordsInKeyPhrase, i)){
+                documentsWithTermFound.add(i);
+            }
+        }
+        return documentsWithTermFound; // place holder
+    }
+    private boolean termFoundInDoc(String[] wordsInKeyPhrase, int docNumber){
+        boolean termFound = false;
+        if(allTermWordsFoundInDoc(wordsInKeyPhrase, docNumber)){
+            List<Integer> wordsInRightOrder = putWordsInTheRightOrder(wordsInKeyPhrase, docNumber);
+            if(wordsInKeyPhrase.length == wordsInRightOrder.size()){
+                //the "putWordsInTheRightOrder" function is designed to under-count
+                //if words show up in a different order than in the search term. We use
+                //this "if" statement to weed out terms in the wrong order
+                termFound = areAllWordsAdjacent(wordsInRightOrder);
+            }
+        }
+        return termFound;
+    }
+    private boolean areAllWordsAdjacent(List<Integer> indexesOfWordsInRightOrderMatched){
+        boolean wordsAreAdjacent = true;
+        for(int i = 0; i < indexesOfWordsInRightOrderMatched.size()-1; i++){
+            if(indexesOfWordsInRightOrderMatched.get(i)+1!=indexesOfWordsInRightOrderMatched.get(i+1)){
+                wordsAreAdjacent = false;
+                break;
+            }
+        }
+        return wordsAreAdjacent;
+    }
+    private List<Integer> putWordsInTheRightOrder(String[] wordsInKeyPhrase, int docNumber){
+        List<Integer> orderOfTermsInDoc = new LinkedList<>();
+        HashMap<Integer, String> indexToStringMap = new HashMap<>();
+        for(int i = 0; i < wordsInKeyPhrase.length; i++){
+            orderOfTermsInDoc.addAll(indexes.get(wordsInKeyPhrase[i]).get(docNumber));
+            for(int g = 0; g < indexes.get(wordsInKeyPhrase[i]).get(docNumber).size(); g++){
+                indexToStringMap.put(indexes.get(wordsInKeyPhrase[i]).get(docNumber).get(g), wordsInKeyPhrase[i]);
+                //associates indexes in each doc w/ search words
+            }
+        }
+        orderOfTermsInDoc.sort(Comparator.naturalOrder());
+        List<Integer> indexesOfWordsInRightOrderMatched = new LinkedList<>();
+        for(int i = 0; i < indexToStringMap.size(); i++){
+            if(indexToStringMap.isEmpty()){
+                break;
+            }
+            if(!indexToStringMap.get(orderOfTermsInDoc.get(i)).equals(wordsInKeyPhrase[i])){
+                indexesOfWordsInRightOrderMatched.clear();
+                indexToStringMap.remove(orderOfTermsInDoc.get(0));
+                orderOfTermsInDoc.remove(0);
+                //if my sequence is {1,2,3,...,n,n+1,n+2,...m}, and
+                //my search term indexes are {n,n+1,n+2}, this function
+                //deletes all {1,...,n-1}, collects {n,n+1,n+2}, and returns it
+                i = -1;
+            }
+            else{
+                indexesOfWordsInRightOrderMatched.add(orderOfTermsInDoc.get(i));
+            }
+            if(indexesOfWordsInRightOrderMatched.size()==wordsInKeyPhrase.length){
+                break;
+            }
+        }
+        return indexesOfWordsInRightOrderMatched;
+    }
+    private boolean allTermWordsFoundInDoc(String[] wordsInKeyPhrase, int docNumber){
+        boolean allTermWordsFound = true;
+        for(int i = 0; i < wordsInKeyPhrase.length; i++){
+            if(!indexes.containsKey(wordsInKeyPhrase[i])){
+                allTermWordsFound = false;
+                break;
+            }
+            else if(indexes.get(wordsInKeyPhrase[i]).get(docNumber).isEmpty()){
+                //if in a document, one of the words in the search term can't be found
+                allTermWordsFound = false;
+                break;
+                //the search term is not in the document
+            }
+        }
+        return allTermWordsFound;
     }
 }
--- src/MyMiniSearchEngineTest.java
+++ src/MyMiniSearchEngineTest.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -22,20 +20,31 @@ public class MyMiniSearchEngineTest {
     public void testOneWord() {
         MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
         List<Integer> result = engine.search("seattle");
-
         assertEquals(1, result.size());
-
         assertEquals(Integer.valueOf(4), result.get(0));
+        result = engine.search("hello");
+        List<Integer> expected = new LinkedList<>();
+        for(int i = 0; i < documents().size(); i = (i==1)? i+2:i+1){
+            expected.add(i);
+        }
+        assertEquals(5, result.size());
+        assertEquals(expected, result);
     }
 
     @Test
     public void testTwoWord() {
         MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
         List<Integer> result = engine.search("hello world");
-
         assertEquals(2, result.size());
-
-        assertEquals(List.of(0, 5), result);
+        List<Integer> expected = new LinkedList<>();
+        expected.add(0);
+        expected.add(5);
+        assertEquals(expected, result);
+        List<Integer> result2 = engine.search("world hello");
+        assertEquals(1, result2.size());
+        List<Integer> expected2 = new LinkedList<>();
+        expected2.add(3);
+        assertEquals(expected2, result2);
     }
 
     @Test
@@ -45,24 +54,53 @@ public class MyMiniSearchEngineTest {
         String[] inputs = {
                 "rains hello abc",
                 "rains Hello abc",
+                "Rains Hello Abc",
+                "raINS HELLo aBC",
         };
-
+        List<Integer> expected = new LinkedList<>();
+        expected.add(4);
         for (String input : inputs) {
             List<Integer> result = engine.search(input);
             assertEquals(1, result.size());
-            assertEquals(List.of(4), result);
+            assertEquals(expected, result);
         }
     }
 
     @Test
     public void testFourWord() {
-        // homework
-        assertTrue(false); // place holder
+        MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
+        String[] inputs = {
+                "sunday hello world fun",
+                "SUNDAY HELLO WORLD FUN",
+                "seattle rains hello ABC"
+        };
+        List<Integer> expected = new LinkedList<>();
+        expected.add(5);
+        for (int i = 0; i < inputs.length; i++) {
+            List<Integer> result = engine.search(inputs[i]);
+            assertEquals(1, result.size());
+            if(i == inputs.length-1){
+                expected.clear();
+                expected.add(4);
+            }
+            assertEquals(expected, result);
+        }
     }
 
     @Test
     public void testWordNotFound() {
-        // homework
-        assertTrue(false); // place holder
+        MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
+        String[] inputs = {
+                "llo wo",
+                "day hell",
+                "seattle rains hell",
+                "money"
+        };
+        List<Integer> expected = new LinkedList<>();
+        for (String input : inputs) {
+            List<Integer> result = engine.search(input);
+            assertEquals(0, result.size());
+            assertEquals(expected, result);
+        }
     }
 }
